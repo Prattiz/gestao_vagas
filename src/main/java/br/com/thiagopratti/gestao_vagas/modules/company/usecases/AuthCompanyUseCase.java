4d5bba04c4889de8bfc9509452456669fd8b2a95
Thiago <prattithiago5@gmail.com>
2024-01-17 14:36:16 -0300

    CREATE AUTH COMPANY RESPONSE DTO

--- src/main/java/br/com/thiagopratti/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
+++ src/main/java/br/com/thiagopratti/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
@@ -2,6 +2,7 @@ package br.com.thiagopratti.gestao_vagas.modules.company.usecases;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 
 import javax.naming.AuthenticationException;
 
@@ -15,6 +16,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.thiagopratti.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.thiagopratti.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import br.com.thiagopratti.gestao_vagas.modules.company.repositories.CompanyRepos;
 
 
@@ -30,7 +32,7 @@ public class AuthCompanyUseCase {
   @Autowired
   private PasswordEncoder passwordEncoder;
 
-  public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+  public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
     var company = this.companyRepos.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
         () -> {
             throw new UsernameNotFoundException("Username/Password: incorrect or not found");
@@ -44,11 +46,21 @@ public class AuthCompanyUseCase {
     }
 
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+    var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
+
+
     var token = JWT.create().withIssuer("javagas")
       .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
       .withSubject(company.getId().toString())
+      .withClaim("roles", Arrays.asList("COMPANY"))
       .sign(algorithm);
 
-      return token;
+      var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
+        .access_token(token)
+        .expires_in(expiresIn.toEpochMilli())
+        .build();
+
+      return authCompanyResponseDTO;
   }
 }
--- src/main/java/br/com/thiagopratti/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/thiagopratti/gestao_vagas/security/SecurityFilter.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -36,20 +37,27 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         if( request.getRequestURI().startsWith("/company") ){
           if( header != null ){
-            var subjectToken = this.jwtProvider.validateToken(header);
+            var token = this.jwtProvider.validateToken(header);
   
-            if( subjectToken.isEmpty() ){
+            if( token == null ){
               response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
               return;
             }
+
+            var roles = token.getClaim("roles").asList(Object.class);
+
+            var grants = roles.stream()
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString()))
+            .toList();
   
-            request.setAttribute("company_id", subjectToken);
+            request.setAttribute("company_id", token.getSubject());
             
-            UsernamePasswordAuthenticationToken auth = new 
+            UsernamePasswordAuthenticationToken auth = 
+            new 
             UsernamePasswordAuthenticationToken(
-              subjectToken, 
+              token.getSubject(), 
               null, 
-              Collections.emptyList()
+              grants
             );
             SecurityContextHolder.getContext().setAuthentication(auth);
           }
