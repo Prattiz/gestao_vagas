ebaec7265dd675aa81e7ed6569a3bcbfad849a3c
Thiago <prattithiago5@gmail.com>
2024-02-02 14:29:05 -0300

    ADD EXAMPLES

--- src/main/java/br/com/thiagopratti/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/thiagopratti/gestao_vagas/modules/company/controllers/JobController.java
@@ -12,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.thiagopratti.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.thiagopratti.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.thiagopratti.gestao_vagas.modules.company.usecases.CreateJobUseCases;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -24,6 +31,19 @@ public class JobController {
 
   @PostMapping("/")
   @PreAuthorize("hasRole('COMPANY')")
+  @Tag(name = "Vacancies", description = "Vacancies's Information")
+  @Operation(
+    summary = "Vacancy registration",
+    description = "This function is responsable to register a vacancy in the company"
+    )
+
+    @ApiResponses(
+        @ApiResponse(responseCode = "200", content = {
+          @Content(schema = @Schema(implementation = JobEntity.class))
+        })
+    )
+
+    @SecurityRequirement(name = "jtw_auth")
   public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
     var companyId = request.getAttribute("company_id");
 
--- src/main/java/br/com/thiagopratti/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/thiagopratti/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,17 @@
 package br.com.thiagopratti.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+  @Schema(example = "Your Schema")
   private String description;
+  
+  @Schema(example = "Your Schema")
   private String benefits;
+
+  @Schema(example = "Your Schema")
   private String level;
 }
